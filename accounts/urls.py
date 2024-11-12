5b1f77410ed9ab59e5a6fd0430ad1d2c7453ddbe
gitsubash44 <dhamisubash866@gmail.com>
2024-11-12 16:57:30 +0545

    adding

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,8 +4,8 @@ from accounts import views
 
 
 urlpatterns = [
+    path('',views.user_login, name="user_login"),
     path('register/',views.user_register, name="user_register"),
-    path('login/',views.user_login, name="user_login"),
 
 
     #manager
